450809bdbf44da2f7957fa649397972e5c7ee838
Pedro Batista <pedromachakio@hotmail.com>
2021-05-24 20:43:39 +0000

    changed static data to dynamic using JS + HTML aka JSX, including built-in Date() and toDateString(), class35

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 18); // contagem começa em Janeiro = 0
+    const expenseTitle = "Bolo pa Maggs";
+    const expenseAmount = 400;
+
   return (
     <div className="expense-item">
-      <div>March 18th 2021</div>
+      <div>{expenseDate.toDateString()}</div> 
       <div className="expense-item__description">
-        <h2 className="expense-item h2">Car Insurance</h2>
-        <div className="expense-item__price">$400</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
