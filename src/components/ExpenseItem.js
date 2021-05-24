03e2b8c89c60a8fcf89166d2b62dd6ad43994a8a
Pedro Batista <pedromachakio@hotmail.com>
2021-05-24 20:15:45 +0000

    added css styling to ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,7 +5,7 @@ function ExpenseItem() {
     <div className="expense-item">
       <div>March 18th 2021</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
+        <h2 className="expense-item h2">Car Insurance</h2>
         <div className="expense-item__price">$400</div>
       </div>
     </div>
