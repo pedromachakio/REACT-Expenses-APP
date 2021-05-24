0261d1bc950b8a09b8a7532cef3dd6538ecb7342
Pedro Batista <pedromachakio@hotmail.com>
2021-05-24 23:16:02 +0000

    Extracted day month and year from the Date() thanks to the built in js toLocaleString and personalized it - also clean up the JSX code by creating 3 variables to hold day/month/year instead and then just writing the pointer in the JSX to the respective one

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,22 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  // o parâmetro não precisa de ser props mas é a convenção
-  /*const expenseDate = new Date(2021, 2, 18); // contagem começa em Janeiro = 0
+  /* o parâmetro não precisa de ser props mas é a convenção
+  const expenseDate = new Date(2021, 2, 18); // contagem começa em Janeiro = 0
     const expenseTitle = "Bolo pa Maggs";
     const expenseAmount = 400;*/
 
+  const month = props.date.toLocaleString("pt-PT", { month: "long" }).toLowerCase(); // fica mais clean numa var à parte do que dentro do JSX code
+  const day = props.date.toLocaleString("pt-PT", { day: "2-digit"});
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
