3c8bc5f1329a5e91cc0d18eec3239c01234057c4
Pedro Batista <pedromachakio@hotmail.com>
2021-05-24 22:42:36 +0000

    lesson 36 - removed the hardcoded data in ExpenseItem.js and added Props so that the 2 components can exchange data and make it reusable. Added array of Expense objects in App.js - the keys of the pairs what we use to then access the values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 18); // contagem começa em Janeiro = 0
+function ExpenseItem(props) {
+  // o parâmetro não precisa de ser props mas é a convenção
+  /*const expenseDate = new Date(2021, 2, 18); // contagem começa em Janeiro = 0
     const expenseTitle = "Bolo pa Maggs";
-    const expenseAmount = 400;
+    const expenseAmount = 400;*/
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div> 
+      <div>{props.date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
       </div>
     </div>
   );
