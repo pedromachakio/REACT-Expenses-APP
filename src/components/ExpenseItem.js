487b25875e51790a2b36f3fb6d1e48d9fdaeb7ad
Pedro Batista <pedromachakio@hotmail.com>
2021-05-27 19:12:17 +0000

    Cleaned up App.js by moving the logic of ExpenseItem to a new Component:Expenses.js. Also added css for the new component -- first assignment

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,14 +2,9 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  /* o parâmetro não precisa de ser props mas é a convenção
-  const expenseDate = new Date(2021, 2, 18); // contagem começa em Janeiro = 0
-    const expenseTitle = "Bolo pa Maggs";
-    const expenseAmount = 400;*/
-
   return (
     <div className="expense-item">
-      <ExpenseDate date={props.date}></ExpenseDate>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2 className>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
