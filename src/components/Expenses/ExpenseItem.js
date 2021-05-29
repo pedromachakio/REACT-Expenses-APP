4493f19628d4cb10f06ffed453d7761025392406
Pedro Batista <pedromachakio@hotmail.com>
2021-05-29 16:01:38 +0000

    added a quick button with a corresponding event listener (using built in Element functions) listening in for clicks (which does nothing yet but print a message)

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,12 +3,14 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const clickHandler = () => console.log("Clicked!!!!");
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2 className>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
+        <button onClick={clickHandler}>Click on me</button>
       </div>
     </Card>
   );
