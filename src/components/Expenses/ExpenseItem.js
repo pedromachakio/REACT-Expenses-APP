227f7e28b5164c9b5a63eb11f5f7d265e92daec6
Pedro Batista <pedromachakio@hotmail.com>
2021-05-29 16:47:13 +0000

    updated the button by using react useState function, thus effectively making the component and the DOM update and display the new content -L48

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  const clickHandler = () => console.log("Clicked!!!!");
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2 className>{props.title}</h2>
+        <h2 className>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
-        <button onClick={clickHandler}>Click on me</button>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
