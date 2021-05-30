3fd6f03bdd4c61b8846dd2371ad068ceecca5c1c
Pedro Batista <pedromachakio@hotmail.com>
2021-05-30 13:56:34 +0000

    added filter to select expenses by year - assignment 2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,40 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import { useState } from "react";
 
 function Expenses(props) {
+  const [yearSelected, setSelectedYear] = useState("2021");
+  function saveYearChangeHandler(selectedYear) {
+    setSelectedYear(selectedYear);
+  }
   return (
-    <Card className="expenses">
-       <ExpenseItem
-        title={props.expenses[0].title}
-        price={props.expenses[0].price}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        price={props.expenses[1].price}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        price={props.expenses[2].price}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        price={props.expenses[3].price}
-        date={props.expenses[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter selectedYear={yearSelected} onYearChange={saveYearChangeHandler} />
+        <ExpenseItem
+          title={props.expenses[0].title}
+          price={props.expenses[0].price}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          price={props.expenses[1].price}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          price={props.expenses[2].price}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          price={props.expenses[3].price}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 
