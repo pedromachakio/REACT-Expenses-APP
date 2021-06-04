707a8dea953246b23edf918f676d4a422a942c48
Pedro Batista <pedromachakio@hotmail.com>
2021-06-04 13:07:54 +0000

    added 2 condition returns on ExpensesList: if there is no expense for the selected year it renders a completely different jsx (1 return per case)

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -2,20 +2,26 @@ import "./ExpensesList.css";
 import ExpenseItem from "./ExpenseItem";
 
 function ExpensesList(props) {
-  let expensesToBeRendered = <p>No Expenses Found for the Selected Year</p>;
-
-  if (props.filteredExpenses.length > 0) {
-    expensesToBeRendered = props.filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        price={expense.price}
-        date={expense.date}
-      />
-    ));
+  if (props.filteredExpenses.length === 0) {
+    return (
+      <h2 className="expenses-list__fallback">
+        No Expenses Found for the Selected Year
+      </h2>
+    );
   }
 
-  return <ul className="expenses-list">{expensesToBeRendered}</ul>;
+  return (
+    <ul className="expenses-list">
+      {props.filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          price={expense.price}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
 }
 
 export default ExpensesList;
